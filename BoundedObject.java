8da9d4751078ee9e4bde02ef4fa54cbf673416c4
Chris Vandevelde <chris.vandevelde@uwaterloo.ca>
2012-03-01 22:16:05 -0800

    Modified RTree.java to work with Android RectFs.

---
+++ BoundedObject.java
@@ -0,0 +1,12 @@
+package com.imlikeaninja.android.utilities.rtree;
+
+import android.graphics.RectF;
+
+/**
+ * An object bounded on an Axis-Aligned Bounding Box.
+ * @author Colonel32
+ * @author cnvandev
+ */
+public interface BoundedObject {
+	public RectF getBounds();
+}
